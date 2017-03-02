7248f1100460aef7e8b887c178342cab435ab948
Alan <akoruth95@gmail.com>
2017-03-01 22:23:56 -0500

    Connected to database and added contact list. Separated header/footer files.

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+
+   include 'database.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Contact Manager</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
--- index.php
+++ index.php
@@ -1,11 +1,22 @@
-<!doctype html>
-  <html>
-    <head>
-      <title>My PHP/MySQL Project</title>
-      <meta name="description" content="Our first page">
-      <meta name="keywords" content="html tutorial template">
-    </head>
-    <body>
-      Content goes here.
-    </body>
-  </html>
+<?php
+
+   include 'header.php';
+   $contacts = $db->query('SELECT * FROM contacts')->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
+<?php foreach($contacts as $contact) : ?>
+  <a href="/edit.php?id=<?= $contact['id']; ?>">
+  <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title"><span class="badge"><?= $contact['id']; ?></span><?= $contact['fname'] . ' ' . $contact['lname']; ?></h3>
+      </div>
+    <div class="panel-body">
+      <?= $contact['city'] . ', ' . $contact['state']; ?>
+    </div>
+  </div>
+  </a>  
+<?php endforeach; ?>
+
+<?php include 'footer.php'; ?>
